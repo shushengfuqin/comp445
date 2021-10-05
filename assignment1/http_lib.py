d2b4b18a79effba3453b8cd0676447e39d6ad7ad
shushengfuqin <ruocjia@gmail.com>
2021-10-05 18:16:16 -0400

    lab 1 push 1

--- assignment1/http_lib.py
+++ assignment1/http_lib.py
@@ -1,37 +1,63 @@
+import http.client
 from urllib.parse import urlparse
 import requests
 import json
+import ast
+import http_lib
+import pprint
 
 
-def GET(url):
+def GET(url,str):
 
     # separate the url in to scheme, netloc, path, paraams, query and fragment
+    pp = pprint.PrettyPrinter(indent = 4)
     link = urlparse(url)
     HOST = link.netloc
     PATH = link.path
     if PATH == "":
         PATH = "/"
-    QUERY = link.querypi
+    QUERY = link.query
 
-    receive = requests.get(url)
-    HEADER = receive.headers
-    BODY = receive.content
+    str = str.split(" ")
+    
+    HEADER = ""
+    start = 0
+    for x in str:
+        start += 1
+        if (x == "-h"):
+            HEADER += str[start] + ""
+    
+    conn = http.client.HTTPConnection(HOST,80)
+    conn.request("GET", PATH, HEADER)
+    receive = conn.getresponse()
 
-    # get the header of the get request into json pretty
-    with open('getheader.json', 'w') as outfile:
-        json.dump(dict(HEADER), outfile, indent=4)
+    print("GET", PATH,"HTTP/",receive.version/10.)
+    print("Host:", HOST,"\n")
+    print(receive.reason, receive.status)
+    
+    if ("-v" in str):
+        pp.pprint(receive.msg)
+    else:
+        pp.pprint(receive.getheaders())
+    
+    pp.pprint(receive.read())
+    
+    receive.close()
+    # # get the header of the get request into json pretty
+    # with open('getheader.json', 'w') as outfile:
+    #     json.dump(dict(HEADER), outfile, indent=4)
 
-    # get the body of the get request into json pretty
-    rbodyjson = receive.json()
+    # # get the body of the get request into json pretty
+    # rbodyjson = receive.json()
 
-    with open('getbody.json', 'w') as outfile:
-        json.dump(rbodyjson, outfile, indent=4)
-
-
-GET("http://httpbin.org/get")
+    # with open('getbody.json', 'w') as outfile:
+    #     json.dump(rbodyjson, outfile, indent=4)
 
 
-def POST(url):
+# GET("http://httpbin.org/get")
+
+
+def POST(url,str):
 
     # separate the url in to scheme, netloc, path, paraams, query and fragment
     link = urlparse(url)
@@ -40,20 +66,32 @@ def POST(url):
     if PATH == "":
         PATH = "/"
     QUERY = link.query
+    if QUERY == "":
+        QUERY = "?"+QUERY
 
     post = requests.post(url)
     HEADER = post.headers
     BODY = post.content
 
-    # get the header of the get request into json pretty
-    with open('postheader.json', 'w') as outfile:
-        json.dump(dict(HEADER), outfile, indent=4)
+    args = str.split(" ")
 
-    # get the body of the get request into json pretty
-    rbodyjson = post.json()
+    body = {}
+    argsLenth = len(args)
+    if ("-d" in args):
+        print("data")
+        a = args.index("-d") + 1
+        body = args[a:argsLenth]
+    elif ("-f" in args):
+        print("file")
+    # # get the header of the get request into json pretty
+    # with open('postheader.json', 'w') as outfile:
+    #     json.dump(dict(HEADER), outfile, indent=4)
 
-    with open('postbody.json', 'w') as outfile:
-        json.dump(rbodyjson, outfile, indent=4)
+    # # get the body of the get request into json pretty
+    # rbodyjson = post.json()
+
+    # with open('postbody.json', 'w') as outfile:
+    #     json.dump(rbodyjson, outfile, indent=4)
 
 
-POST("http://httpbin.org/post")
+# POST("http://httpbin.org/post")
--- assignment1/httpc.py
+++ assignment1/httpc.py
@@ -1,10 +1,27 @@
 import sys
-import argparse
-# from http_lib import GET,POST
+from http_lib import GET,POST
 
 def main(kb):
-    print("input")
-    print(kb)
+    method = kb[0]
+    print(method)
+    url = kb[-1]
+    print(url)
+    del kb[0], kb[-1]
+    body = " ".join(kb)
+    print(body)
+
+    if method == "GET":
+        if "-d" not in body and "-f" not in body:
+            print(f"[GET REQUEST] {body}")
+            GET(url,body)
+        else:
+            print("\nError : GET REQUEST SHALL NOT BE USED WITH OPTIONS -d or -f")
+    elif method == "POST":
+        if (("-d" in body) or ("-f" in body)) is not (("-d" in body) and ("-f" in body)):
+            print(f"[POST REQUEST] {body}")
+            POST(url,body)
+        else:
+            print("\nError : POST REQUEST SHALL NOT USING -d and -f at the same time")
 
 if __name__ == "__main__":
     main(sys.argv[1:])
