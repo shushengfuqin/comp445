d3ca0e278942a6ca5af9c31da330d21bfbd54a9b
Jin <ruocjia@gmail.com>
2021-10-06 14:48:33 -0400

    main finished

--- assignment1/http_lib.py
+++ assignment1/http_lib.py
@@ -2,15 +2,12 @@ import http.client
 from urllib.parse import urlparse
 import requests
 import json
-import ast
-import http_lib
-import pprint
 
 
-def GET(url,str):
+def GET(url, str):
 
     # separate the url in to scheme, netloc, path, paraams, query and fragment
-    pp = pprint.PrettyPrinter(indent = 4)
+
     link = urlparse(url)
     HOST = link.netloc
     PATH = link.path
@@ -18,80 +15,46 @@ def GET(url,str):
         PATH = "/"
     QUERY = link.query
 
-    str = str.split(" ")
-    
-    HEADER = ""
-    start = 0
-    for x in str:
-        start += 1
-        if (x == "-h"):
-            HEADER += str[start] + ""
-    
-    conn = http.client.HTTPConnection(HOST,80)
-    conn.request("GET", PATH, HEADER)
-    receive = conn.getresponse()
 
-    print("GET", PATH,"HTTP/",receive.version/10.)
-    print("Host:", HOST,"\n")
-    print(receive.reason, receive.status)
-    
+    receive = requests.get(url)
+    HEADER = receive.headers
     if ("-v" in str):
-        pp.pprint(receive.msg)
+        for key, value in receive.headers.items():
+            print(key, ":", value)
+        print(receive.text)
+        with open('data.txt', 'w') as outfile:
+            json.dump(dict(receive.headers),outfile, indent = 4)
+            outfile.write(receive.text)
+        outfile.close
     else:
-        pp.pprint(receive.getheaders())
-    
-    pp.pprint(receive.read())
-    
+        print(receive.text)
+        with open('data.txt', 'w') as outfile:
+            outfile.write(receive.text)
+        outfile.close
+
     receive.close()
-    # # get the header of the get request into json pretty
-    # with open('getheader.json', 'w') as outfile:
-    #     json.dump(dict(HEADER), outfile, indent=4)
-
-    # # get the body of the get request into json pretty
-    # rbodyjson = receive.json()
-
-    # with open('getbody.json', 'w') as outfile:
-    #     json.dump(rbodyjson, outfile, indent=4)
 
 
-# GET("http://httpbin.org/get")
 
+def POST(url, str):
 
-def POST(url,str):
-
-    # separate the url in to scheme, netloc, path, paraams, query and fragment
-    link = urlparse(url)
-    HOST = link.netloc
-    PATH = link.path
-    if PATH == "":
-        PATH = "/"
-    QUERY = link.query
-    if QUERY == "":
-        QUERY = "?"+QUERY
-
-    post = requests.post(url)
-    HEADER = post.headers
-    BODY = post.content
-
+    #process the string
     args = str.split(" ")
+    
+    BODY = {}
+    argsLenth = len(str)
+    if ("--d" in args):
+        a = args.index("--d") + 1
+        BODY = args[a:argsLenth]
+        print(BODY)
+    elif ("--f" in args):
+        file = open(args[args.index("--f")+1],"r")
+        BODY = (file.read())
 
-    body = {}
-    argsLenth = len(args)
-    if ("-d" in args):
-        print("data")
-        a = args.index("-d") + 1
-        body = args[a:argsLenth]
-    elif ("-f" in args):
-        print("file")
-    # # get the header of the get request into json pretty
-    # with open('postheader.json', 'w') as outfile:
-    #     json.dump(dict(HEADER), outfile, indent=4)
+    post = requests.post(url,json = BODY)
 
-    # # get the body of the get request into json pretty
-    # rbodyjson = post.json()
-
-    # with open('postbody.json', 'w') as outfile:
-    #     json.dump(rbodyjson, outfile, indent=4)
+    print(post.text)
 
 
-# POST("http://httpbin.org/post")
+
+
