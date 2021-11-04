2fc3806b0e94ab879b0ec55e6fd2f40cefc5e07c
Jin <ruocjia@gmail.com>
2021-11-04 15:12:08 -0400

    lab2 first step

--- assignment2/https.py
+++ assignment2/https.py
@@ -1,15 +1,14 @@
-import os
 import argparse
+import os
 import socket
 import re
 
-
-def runHttpfs(port, directory):
+def server(port, dir):
     host = ''
-    localdir = directory
-    if directory is None:
+    localdir = dir
+    if dir is None:
         localdir = os.path.dirname(os.path.realpath(__file__))
-
+    
     listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     listener.bind((host, port))
@@ -20,65 +19,66 @@ def runHttpfs(port, directory):
         files = os.listdir(localdir)
         conn, addr = listener.accept()
         request = conn.recv(1024).decode("utf-8")
-        # breaking up the request
+        #breaking up the request
         request = request.split('\r\n')
         mpv = request[0].split()
         method = mpv[0]
         path = mpv[1]
 
-        index = request.index('')
+        index = request.index('') 
         data = ''
         for l in request[index+1:]:
             data += l + '\n'
 
         response = ''
-
+        
         if method == 'GET':
             if path == '/':
                 if args.verbose:
-                    print('Responding with list of files')
+                    print ('Responding with list of files')
                 for f in files:
                     response += f + '\n'
             elif re.search(r'\/\w+.\w+', path):
                 path = path.strip('/')
                 if args.verbose:
-                    print('Responding with contents of', path)
+                    print ('Responding with contents of', path)
                 if path in files:
-                    theFile = open(localdir + '/' + path, 'r')
+                    theFile = open(localdir +'/'+ path, 'r')
                     response = theFile.read() + '\n'
                     theFile.close()
                 else:
                     if args.verbose:
-                        print('Responding with HTTP 404 - file(s) not found', path)
+                        print ('Responding with HTTP 404 - file(s) not found', path)
                     response = 'HTTP 404 - file(s) not found\n'
         elif method == 'POST':
             path = path.strip('/')
             if path in files:
                 if args.verbose:
-                    print('Responding with data overwritten to file', path)
-                theFile = open(localdir + '/' + path, 'w+')
+                    print ('Responding with data overwritten to file', path)
+                theFile = open(localdir +'/'+ path, 'w+')
                 theFile.write(data)
                 theFile.close()
                 response = 'Data overwritten to file '+path
             elif path not in files:
                 if args.verbose:
-                    print('Responding with data written to new file ' + path)
-                theFile = open(localdir + '/' + path, 'w+')
+                    print ('Responding with data written to new file '+ path)
+                theFile = open(localdir +'/'+ path, 'w+')
                 theFile.write(data)
                 theFile.close()
-                response = 'Data written to new file ' + path
+                response = 'Data written to new file '+ path
             else:
                 if args.verbose:
-                    print('Responding with HTTP 403 - action refused')
+                    print ('Responding with HTTP 403 - action refused')
                 response = "HTTP 403 - action refused \n"
 
         conn.sendall(response.encode('utf-8'))
         conn.close()
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-v', '--verbose', help='verbosity', action='store_true')
-parser.add_argument('-p', '--port', help='Spcify port - Default is 8080', type=int, default=8080)
-parser.add_argument('-d', '--directory', help='Specify directory - Default is current', type=str)
+
+parser = argparse.ArgumentParser(description="http fileserver")
+parser.add_argument('-v', '--verbose', help = 'Prints debugginng messages', action='store_true')
+parser.add_argument('-p', '--port', type = int, default= 8080,help='Specifies the port number that the server will listen and serve at.\n\n Default is 8080')
+parser.add_argument('-d', '--directory', type = str, help='Specifies the directory that the server will use to read/write requested files. Default is the current directory when launching the application.')
 args = parser.parse_args()
-runHttpfs(args.port, args.directory)
+server(args.port,args.directory)
